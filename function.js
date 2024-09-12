01ba9d0de89e56b7df7a1594873c01c88007c8e7
Lőrintz Levente <lorintz.levente@burg.lo.hu>
2024-09-12 08:29:18 +0200

    Util function factorial.

---
+++ function.js
@@ -0,0 +1,21 @@
+function getFirstElement (arr){
+    if (arr) {
+        return arr[0]; 
+    }
+    return null
+    
+    }
+    const numbers = [1, 2, 3];
+    const words = ["apple","banana","cherry"];
+    console.log(`First number: ${getFirstElement(numbers)}`);
+    console.log(`First word: ${getFirstElement(words)}`);
+
+    export function factorial(n) {
+        if (n <= 1) {
+            return;
+        }
+        return factorial(n-1)
+        }
+        
+        console.log(`Factorial of 5: ${factorial(5)}`);
+        
